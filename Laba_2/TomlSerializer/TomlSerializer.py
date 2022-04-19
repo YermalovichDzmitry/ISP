90e8b55e6780b5038a2beb531667ce32ce953cec
Ermolovich Dima <ermolovichdima22@gmail.com>
2022-04-19 21:26:36 +0300

    19.04.2022 9:25

--- Laba_2/TomlSerializer/TomlSerializer.py
+++ Laba_2/TomlSerializer/TomlSerializer.py
@@ -25,38 +25,3 @@ class TomlSerializer:
         with open(filename, "rb") as f:
             dict_obj = tomli.load(f)
         return DictSerializer.deserialize(dict_obj)
-
-# class TomlSerializer:
-#     @staticmethod
-#     def dump(obj, filename):
-#         dict_obj = DictSerializer.serialize(obj)
-#         toml_parser_1(obj, filename)
-#         obj_tuple_ser = Serializer.serialize(obj)
-#
-#         str_tuple = Serializer.tuple_to_str(obj_tuple_ser)
-#         tuple_filename = filename + "_tuple.txt"
-#         with open(tuple_filename, "w") as f:
-#             f.write(str_tuple)
-#
-#     @staticmethod
-#     def dumps(obj):
-#         dict_obj = DictSerializer.serialize(obj)
-#         filename = "../data_for_dumps.txt"
-#         toml_parser_1(dict_obj, filename)
-#         with open(filename, "r") as f:
-#             toml_ser = f.read()
-#
-#         return toml_ser, dict_obj
-#
-#     @staticmethod
-#     def loads(obj):
-#         return DictSerializer.deserialize(obj)
-#
-#     @staticmethod
-#     def load(filename):
-#         tuple_filename = filename + "_tuple.txt"
-#         with open(tuple_filename, "r") as f:
-#             str_obj = f.read()
-#
-#         tuple_obj = Serializer.str_to_tuple(str_obj)
-#         return Serializer.deserialize(tuple_obj)
